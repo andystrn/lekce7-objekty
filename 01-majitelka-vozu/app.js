a0d0fea2562300ac1ede08d10d042ee6f7514376
Andrea Strnadlova <an.strnadlova@gmail.com>
2022-04-08 11:04:08 +0200

    added object auto

--- 01-majitelka-vozu/app.js
+++ 01-majitelka-vozu/app.js
@@ -1,10 +1,15 @@
 // Vytvoř objekt auto, který bude mít vlastnosti: znacka, barva, rokVyroby,..
-
+let auto = {
+    znacka: 'Ford';
+    barva: 'modra';
+    rokVyroby: 2015;
+    spz: '182 356';
+};
 
 // Přidej novou vlastnost (majitel a prirad ji svoje jmeno jako hodnotu)
-
+auto.majitel = 'Andrea';
 
 // Přepiš značku na Škoda
-
+auto.znacka = 'Skoda';
 
 // Vytvor tri promenne majitelka, znacka a spz a vloz do techto elementu text z objektu auto.
