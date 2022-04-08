c1e1a0bf5ea73496e154b4ae3f189a53299fa6b2
Andrea Strnadlova <an.strnadlova@gmail.com>
2022-04-08 11:11:52 +0200

    car owner data structure

--- 01-majitelka-vozu/app.js
+++ 01-majitelka-vozu/app.js
@@ -1,15 +1,20 @@
 // Vytvoř objekt auto, který bude mít vlastnosti: znacka, barva, rokVyroby,..
 let auto = {
-    znacka: 'Ford';
-    barva: 'modra';
-    rokVyroby: 2015;
-    spz: '182 356';
+    znacka: 'Ford',
+    barva: 'modra',
+    rokVyroby: 2015,
+    spz: '182 356',
 };
 
 // Přidej novou vlastnost (majitel a prirad ji svoje jmeno jako hodnotu)
-auto.majitel = 'Andrea';
+auto.majitel = {};
+auto.majitel.jmeno = 'Jana';
+auto.majitel.prijimeni = 'Czechitas';
 
 // Přepiš značku na Škoda
 auto.znacka = 'Skoda';
 
 // Vytvor tri promenne majitelka, znacka a spz a vloz do techto elementu text z objektu auto.
+let majitelka = auto.majitel.jmeno + ' ' + auto.majitel.prijimeni;
+let znacka = auto.znacka;
+let spz = auto.spz;
